abe714647f3cebe3919103969de6fb8f04dde495
Sergey Bogomolov <bogomolovs693@gmail.com>
2025-05-25 21:40:20 +0300

    add scheduled_tasks to migrations

--- migrations/000001_initial.up.sql
+++ migrations/000001_initial.up.sql
@@ -3,12 +3,13 @@ CREATE TYPE frequency AS ENUM('daily', 'weekly', 'monthly', 'quarterly', 'half_y
 CREATE TYPE sub_status AS ENUM('active', 'cancelled', 'trial', 'expired');
 CREATE TYPE notification_status AS ENUM('sent', 'failed');
 CREATE TYPE notification_type AS ENUM('payment_reminder', 'otp', '2fa', 'email_change', 'weekly_summary');
+CREATE TYPE task_status AS ENUM('pending', 'completed', 'failed');
 
 CREATE TABLE users (
     user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
-    password_hash BYTEA,
-    google_id TEXT UNIQUE,
+    password_hash BYTEA, -- если гугл - будет null
+    google_id TEXT UNIQUE, -- если только пароль - будет null
     is_email_verified BOOLEAN DEFAULT FALSE,
     avatar_url TEXT,
     full_name TEXT,
@@ -17,11 +18,11 @@ CREATE TABLE users (
 );
 
 CREATE TABLE email_otps (
-  user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
-  code CHAR(6) NOT NULL,
-  expires_at TIMESTAMP NOT NULL,
-  created_at TIMESTAMP DEFAULT now(),
-	PRIMARY KEY (user_id, code)
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    code CHAR(6) NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    PRIMARY KEY (user_id, code)
 );
 
 CREATE TABLE subscriptions (
@@ -60,5 +61,15 @@ CREATE TABLE notifications (
     status notification_status NOT NULL,
     sent_at TIMESTAMP,
     metadata JSONB,
+    created_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE scheduled_tasks (
+    task_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INT NOT NULL,
+    task_type TEXT NOT NULL, -- например: 'payment_reminder', 'autopayment', 'weekly_summary'
+    payload JSONB, -- данные, которые понадобятся исполнителю задачи
+    run_at TIMESTAMP NOT NULL, -- когда нужно запустить
+    status task_status NOT NULL DEFAULT 'pending', -- pending, processing, completed, failed
     created_at TIMESTAMP DEFAULT now()
 );
